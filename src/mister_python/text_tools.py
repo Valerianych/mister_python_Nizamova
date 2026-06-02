f1bffffb6797f308b2e59a027ca053a1c659fdcf
Valerianych <82865796+Valerianych@users.noreply.github.com>
2026-06-02 10:20:22 +0500

    Complete text tools

--- src/mister_python/text_tools.py
+++ src/mister_python/text_tools.py
@@ -1,13 +1,14 @@
 def normalize_text(text):
-    # TODO: remove spaces around text and convert to lower case
-    pass
+    result = text.strip().lower()
+    return result
 
 
 def split_words(text):
-    # TODO: split text into words
-    pass
+    result = text.split()
+    return result
 
 
 def has_word(text, word):
-    # TODO: check that word exists in text
-    pass
+    words = split_words(text)
+    result = word in words
+    return result
