7830fd8f854736f6cb8b9b4b89d18b7f313f2d4b
Valerianych <82865796+Valerianych@users.noreply.github.com>
2026-06-02 10:17:51 +0500

    Add text tools template

---
+++ src/mister_python/text_tools.py
@@ -0,0 +1,13 @@
+def normalize_text(text):
+    # TODO: remove spaces around text and convert to lower case
+    pass
+
+
+def split_words(text):
+    # TODO: split text into words
+    pass
+
+
+def has_word(text, word):
+    # TODO: check that word exists in text
+    pass
