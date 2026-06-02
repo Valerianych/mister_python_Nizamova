8dcde041aff56ee417d0a1cff38ccb5d10f71800
Valerianych <82865796+Valerianych@users.noreply.github.com>
2026-06-02 10:17:34 +0500

    Add math tools template

---
+++ src/mister_python/math_tools.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    # TODO: return sum of two numbers
+    pass
+
+
+def subtract(a, b):
+    # TODO: return difference of two numbers
+    pass
+
+
+def multiply(a, b):
+    # TODO: return product of two numbers
+    pass
+
+
+def divide(a, b):
+    # TODO: check zero divisor and return division result
+    pass
+
+
+def is_even(number):
+    # TODO: return True if number is even
+    pass
